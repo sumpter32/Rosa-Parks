28ac1f42de3931b637578fb515ff969a619a77c7
Steven S <sumpter32@gmail.com>
2023-04-24 15:51:46 +0800

    update greeting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,7 +83,7 @@
   <div class="flex flex-col w-full px-0 items-center h-full">
 	<div class="chat-container w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
 	  <div class="flex flex-col gap-2">
-		<ChatMessage type="assistant" message="What do you want!" />
+		<ChatMessage type="assistant" message="Hello! I am Rosa Parks, the civil rights activist who stood up against segregation. Let's talk about the ongoing struggle for equality and justice." />
 		{#each chatMessages as message}
 		  <ChatMessage type={message.role} message={message.content} />
 		{/each}
